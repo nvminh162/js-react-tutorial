5d954074fe16c3b3898aab5d0b2c4b081d0c4d65
Nguyen Van Minh <121565657+nvminh162@users.noreply.github.com>
2025-04-28 19:46:36 +0700

    push redux core working thunk middleware

---
+++ reactjs/redux-ultimate/redux-core-1/redux-core-middleware-thunk/README.md
@@ -0,0 +1,10 @@
+#20
+Note for redux
+
+npm i redux react-redux
+
+npm i @redux-devtools/extension
+
+npm i react-bootstrap 
+
+npm i redux-thunk
---
+++ reactjs/redux-ultimate/redux-core-1/redux-core-middleware-thunk/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import TableUser from "./TableUser";
+import FormAddNew from "./FormAddNew";
+
+const Home = (props) => {
+  return (
+    <>
+      <Header />
+      <FormAddNew />
+      <TableUser />
+    </>
+  );
+};
+
+export default Home;
